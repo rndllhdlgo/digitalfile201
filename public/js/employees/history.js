39e23f5f5c736cebb8dfd6794ae984b2e2779fd6
rndllhdlgo <hidalgorendell20@gmail.com>
2023-05-11 13:49:25 +0800

    DELETED JS FILES

--- public/js/employees/history.js
+++
@@ -1,8 +0,0 @@
-$('.employee_history_table tbody').on('click', 'tr', function(){
-    var data =  $('.employee_history_table').DataTable().row(this).data();
-    Swal.fire({
-        title: `<h5>` + moment(data.date).format('dddd, MMMM DD, YYYY, h:mm:ss A') + `</h5>`,
-        html: `<ol style="text-align: left !important;font-weight:600 !important;">` +  data.history.replaceAll(" [","<li>[") + `</li></ol>`,
-        width: 850,
-    });
-});
