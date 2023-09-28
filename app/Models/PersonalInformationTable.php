8a67eda1f2fcd8ee366a72d250d62c65480d7f02
rndllhdlgo <hidalgorendell20@gmail.com>
2023-09-28 12:32:03 +0800

    Original

--- app/Models/PersonalInformationTable.php
+++ app/Models/PersonalInformationTable.php
@@ -8,8 +8,4 @@ class PersonalInformationTable extends Model
 {
     protected $guarded = [];
     protected $table = 'personal_information_tables';
-
-    public function work_information(){
-        return $this->belongsTo(WorkInformationTable::class,'id','employee_id');
-    }
 }
--- public/js/employees/employees.js
+++ public/js/employees/employees.js
@@ -91,13 +91,13 @@ $(document).ready(function(){
                     return (row.last_name + ', ' + row.first_name + ' ' + row.middle_name).toUpperCase();
                 }
             },
-            {data: 'work_information.position.job_position_name'},
-            {data: 'work_information.branch.entity03_desc'},
-            {data: 'work_information.employment_status'},
-            {data: 'work_information.company.company_name'},
-            {data: 'work_information.department.deptdesc'},
+            {data: 'employee_position'},
+            {data: 'employee_branch'},
+            {data: 'employment_status'},
+            {data: 'employee_company'},
+            {data: 'employee_department'},
             {
-                data: 'work_information.date_hired',
+                data: 'date_hired',
                 "render":function(data,type,row){
                     if(row.date_hired){
                         return formatDate(row.date_hired);
